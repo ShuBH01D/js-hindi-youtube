beb417c55ed292a41c0dd6048bbb020bf516f631
shubham <162412805+ShuBH01D@users.noreply.github.com>
2024-07-11 13:05:13 +0000

    addd file

---
+++ chaiorcode.js
@@ -0,0 +1,11 @@
+const accountId=21323243452345
+let accountEmail="shbhamdighe"
+var accountPassword="12345"
+account="jaipur"
+
+// accountId=131232322323
+accountEmail="fjvnxjvzx"
+accountPassword="123234234"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,account])
---
+++ datatype.js
@@ -0,0 +1,19 @@
+//alert (3+3)//we are using nodejs, not browser
+let name="shubham" 
+let age=18
+let isloggedIn
+
+
+//number =>2 to power 53
+//bigint 
+//tring => ""
+//boolean => true/false
+//undefined =>
+//null=>standolone vlue
+
+
+
+
+//object
+
+console.log(typeOfnulle);
