d2371e9c6d900ff329dabdfd648aed08938cefc4
shubham <162412805+ShuBH01D@users.noreply.github.com>
2024-07-12 08:22:53 +0000

    addd

--- datatype.js
+++ datatype.js
@@ -1,19 +1,52 @@
-//alert (3+3)//we are using nodejs, not browser
-let name="shubham" 
-let age=18
-let isloggedIn
+// //alert (3+3)//we are using nodejs, not browser
+// let name="shubham" 
+// let age=18
+// let isloggedIn
 
 
-//number =>2 to power 53
-//bigint 
-//tring => ""
-//boolean => true/false
-//undefined =>
-//null=>standolone vlue
+// //number =>2 to power 53
+// //bigint 
+// //tring => ""
+// //boolean => true/false
+// //undefined =>
+// //null=>standolone vlue
 
 
 
 
-//object
+// //object
 
-console.log(typeOfnulle);
+// console.log(typeOfnulle);
+
+// const name ="shubham"
+// const repocount=50
+
+// console.log(`hello my name is ${name} and my repo count is ${repocount} `);
+
+
+const company=new String('midknight')
+
+console.log(company);
+
+
+// console.log(company.length);
+
+// console.log(company.charAt(2));
+
+// console.log(company.indexOf('m'));
+
+
+const newstring=company.substring(0,4)
+
+console.log(newstring);
+
+
+const wstring=company.slice(2,4 );
+ console.log(wstring);
+
+
+const mkd="     ssfsgsg    "
+ console.log(mkd);
+console.log(mkd.trim());
+
+ 
