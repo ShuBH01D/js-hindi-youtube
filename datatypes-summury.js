0329533858d4dcb406eae230cbfdcdc08325b404
shubham <162412805+ShuBH01D@users.noreply.github.com>
2024-07-12 06:06:12 +0000

    data type summury

---
+++ datatypes-summury.js
@@ -0,0 +1,13 @@
+const heros=["shubham","nitish","sudarshan"]
+let myobj={
+        name:"shubh",
+        age:24,
+}
+
+const myFunction= function(){
+    console.log("hello world");
+}
+
+myFunction()
+
+console.log(typeof myFunction);
