74f38fd723a9261e847e6b10dd435454d0cf99b1
shubham <162412805+ShuBH01D@users.noreply.github.com>
2024-07-16 13:26:59 +0000

    complete

---
+++ array.js
@@ -0,0 +1,27 @@
+const marvel_hearoes=['thor','ironman','spiderman']
+const dc_heros = ['superman','flash','batman']
+
+
+// marvel_hearoes.push(dc_heros)
+// console.log(marvel_hearoes);
+
+
+//spread operartor 
+
+
+// const all_new=[...marvel_hearoes,...dc_heros]
+// console.log(all_new);
+
+const another_anothr=[1,2,3,[4,5,6],7,8,9]
+const realanother_array=another_anothr.flat(Infinity)
+// console.log(realanother_array); 
+
+
+
+// console.log((Array.isArray("shubham"))
+ let score1=100
+ let score2=200
+ let score3=400
+
+
+ console.log(Array.of(score1,score2,score3));
---
+++ objects.js
@@ -0,0 +1,15 @@
+//singleton
+//object literls 
+
+
+const jsuser={
+    name :"shubham",
+    age:24,
+    location:"nashik", 
+    email:"shubhamdighe@123.com",
+    days:["monday","tuesday"]
+    
+}
+// console.log(jsuser.email);
+// console.log(jsuser["email"])
+
